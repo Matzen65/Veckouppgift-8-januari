fe1b114df3280195b34c158ea464183b8136104a
Matzen65 <matzeliasson@gmail.com>
2025-01-10 15:32:05 +0100

    veckouppgift 3

---
+++ .venv/veckouppgift3.py
@@ -0,0 +1,9 @@
+
+# 3 använda variabler och datatyper
+
+tal1 = int(input("ange ett heltal"))
+tal2 = int(input("ange ett annat heltal"))
+summa = (tal1+tal2)
+print(summa)
+
+# Då input levererar en sträng måste man ange att inmatningen är ett tal med int, ger ett (Integer)
