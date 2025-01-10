fdf87b0ec27dd4497a2d56a9f69c4fd2c3030b00
Matzen65 <matzeliasson@gmail.com>
2025-01-10 15:11:01 +0100

    version 2

--- .venv/veckouppgift2.py
+++ .venv/veckouppgift2.py
@@ -1,9 +1,11 @@
-x = 100 # biljettpris
-y = 200 # pengar på fickan
 
-print("Det blir ", (y-x), " kronor över.")
-z = 200 - 100 / 2
-print("hälften är: ", z)
+pris = 100 # biljettpris
+fickpengar = 200 # pengar på fickan
+halva_pris = 0 #hälften av priset
+
+print("Det blir ", (fickpengar - pris), " kronor över.")
+halva_pris = (fickpengar - pris / 2)
+print("hälften är: ", halva_pris)
 
 # Koden ville först addera (y-x) till strängen, detta funkade ej då (y-x) var en float.
 # En sträng kan bara addera en sträng, inte float, därför byter vi + mot komma i koden.
